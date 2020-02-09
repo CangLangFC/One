0ea5724f2faa03a77af1cfce519e2be18a176424
CangLangFC <51995388+CangLangFC@users.noreply.github.com>
2020-02-09 23:06:05 +0800

    Update python的关键要素.py
    mi

--- python的关键要素.py
+++ python的关键要素.py
@@ -17,3 +17,42 @@ ECN.append("greece")
 print(ECN)
 
 
+#1.2.4 逻辑操作符
+a="1234"
+b=None
+a is b
+
+# == 等于  ！= 不等于  
+
+# in greece in ECN.
+
+# and, or, not.
+
+
+#1.2.5 控制流语句
+    #if  while
+
+a1=15000
+if a1<1000:
+    print("small")
+elif a1<10000:
+    print("medium")
+else:
+    print("large")
+
+for country in ECN:
+    print(country)
+
+
+#1.2.5.4
+
+
+#1.2.6 算术操作符
+print(12/3)
+
+a=8
+a+=8  #相当于a=a+8
+print(a)
+
+ECN+=["estonia"]
+print(ECN)
