64e88c38fcc7578421cbbc331a9f291dd0cc3fc7
CangLangFC <51995388+CangLangFC@users.noreply.github.com>
2020-02-13 22:54:44 +0800

    Create if语句.py

---
+++ if语句.py
@@ -0,0 +1,35 @@
+#and, or.
+#in , not in检查是否在/
+
+age=17
+if age<4:
+    print("free")
+elif age<18:
+    print("$5")
+else:
+    print("$10")
+
+alien_color=['red']
+if 'yellow' in alien_color:
+    print("add 5")
+elif 'green' in alien_color:
+    print("add 10")
+else:
+    print("add 15")
+
+
+age=67
+if age<2:
+    print("婴儿")
+elif age<4:
+    print("蹒跚学步")
+elif age<13:
+    print("儿童")
+elif age<20:
+    print("青少年")
+elif age<25:
+    print("成年")
+else:
+    print("老年人")
+
+
